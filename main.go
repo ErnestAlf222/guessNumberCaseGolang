2b5bbffddba0d32bf55d0922b8466b37d3bdbcd7
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-06-18 18:04:14 -0600

    uso del paquete time

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	t := time.Now()
+	hora := t.Hour()
+
+	if hora < 12 {
+		fmt.Println("Es de mañana")
+
+	} else if hora < 17 {
+		fmt.Println("Es tarde")
+	} else {
+		fmt.Println("Es noche")
+
+	}
 
 }
